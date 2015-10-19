35b223e9c224b8b7b849330116d91e983ffe9a0c
Victor Elias <victorgelias@gmail.com>
2015-10-19 10:38:38 -0200

    Exporting JS from 2048 as node.js module
    With some extra needed utilities

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ var path       = require('path');
 
 var port = process.env.PORT || 8080;
 
-var dir2048 = path.resolve(__dirname + '/../2048');
+var dir2048 = path.join(__dirname, '..', '2048');
 
-app.use('/js',    express.static(dir2048 + '/js'));
-app.use('/style', express.static(dir2048 + '/style'));
+app.use('/js',    express.static(path.join(dir2048, 'js')));
+app.use('/style', express.static(path.join(dir2048, 'style')));
 
 app.get('/', function(req, res) {
   console.log("Serving 2048's index.html");
-  res.sendFile(dir2048 + '/index.html');
+  res.sendFile(path.join(dir2048, 'index.html'));
 });
 
 app.listen(port);
---
+++ server/core2048.js
@@ -0,0 +1,50 @@
+var fs    = require('fs');
+var path  = require('path');
+var vm    = require('vm');
+
+var jsdir = path.join(__dirname, '..', '2048', 'js');
+var sandbox = vm.createContext();
+
+function execFile(filename, sandbox) {
+  var filepath = path.join(jsdir, filename);
+  var code = fs.readFileSync(filepath).toString();
+  vm.runInContext(code, sandbox);
+}
+
+execFile('tile.js', sandbox);
+execFile('grid.js', sandbox);
+execFile('game_manager.js', sandbox);
+
+exports.Tile = sandbox.Tile;
+exports.Grid = sandbox.Grid;
+
+function createDummyClass() {
+  var dummyClass = function(){};
+  for (var i = 0; i < arguments.length; i++) {
+    dummyClass.prototype[arguments[i]] = function(){};
+  }
+  return dummyClass;
+}
+
+exports.DummyInputManager = createDummyClass('on');
+exports.DummyActuator = createDummyClass('actuate', 'continueGame');
+exports.DummyStorageManager = createDummyClass('getBestScore', 'setBestScore', 'getGameState', 'setGameState', 'clearGameState');
+
+// Add default arguments to GameManager constructor
+function GameManager(size, InputManager, Actuator, StorageManager) {
+  sandbox.GameManager.call(this, size, 
+                                 InputManager || exports.DummyInputManager, 
+                                 Actuator || exports.DummyActuator, 
+                                 StorageManager || exports.DummyStorageManager);
+}
+GameManager.prototype = Object.create(sandbox.GameManager.prototype);
+GameManager.prototype.constructor = GameManager;
+
+exports.GameManager = GameManager;
+
+exports.Direction = Object.freeze({
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 2,
+  LEFT: 3
+});
